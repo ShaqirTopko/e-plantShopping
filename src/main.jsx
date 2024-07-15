cbebb1833f04e13895b71b51882328b8cf029693
ShaqirTopko <topkoshaqir@gmail.om>
2024-07-15 15:43:52 -0400

    Configured Redux store in store.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store'; // Importo store
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CreatSlice';
 
- const store = configureStore({
-    reducer: {
-    },
+// Konfigurimi i Redux store
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
 });
-export default store
+
+// Eksporto Redux store
+export default store;
